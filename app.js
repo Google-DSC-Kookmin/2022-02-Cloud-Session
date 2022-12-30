af8da1bf791cc4fd31926f26b80fefd244481013
SeYeol_Park <79959576+SeYeol00@users.noreply.github.com>
2022-12-30 16:51:47 +0900

    fix: 1번과제의 6번 수행

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  res.send('#1 과제 수행 완료')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log('창훈님 사랑해요')
 })
+
+// 안녕하세요 창훈님 제가 노드를 몰라서 부딕이하게 커밋을 이런 걸로 하게 되네요 ㅋㅋ 정상적으로 작동 합니다!!
+
+/**
+ * 시간 되면 노드도 공부 해볼게요
+ */
