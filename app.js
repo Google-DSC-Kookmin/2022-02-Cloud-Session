433e8ad09aa291cc8296b3fad1598c52be18c91c
SeYeol_Park <79959576+SeYeol00@users.noreply.github.com>
2022-12-30 17:40:53 +0900

    add: workflow.yml 파일

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-  res.send('#1 과제 수행 완료')
+})
+
+app.get('/api', (req,res) => {
+  res.send('1번과제 완료')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   console.log('창훈님 사랑해요')
+  console.log('버전 3.0')
 })
 
 // 안녕하세요 창훈님 제가 노드를 몰라서 부딕이하게 커밋을 이런 걸로 하게 되네요 ㅋㅋ 정상적으로 작동 합니다!!
